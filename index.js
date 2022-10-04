25c4ede339ce8a0102e59b483844d50d3f42f485
Jordan <43437534+Jaardy@users.noreply.github.com>
2022-10-04 21:39:08 +0100

    testing tests

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express();
-const { Joke } = require('./db');
+const { Joke, Sequelize } = require('./db');
+const { Op } = Sequelize;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/jokes', async (req, res, next) => {
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const where = {};
+
+    if (req.query.tags) {
+      where.tags = { [Op.like]: `%${req.query.tags}%` }; // search within the string, not only exact matches
+    }
+
+    if (req.query.content) {
+      where.joke = { [Op.like]: `%${req.query.content}%` }; // search within the string, not only exact matches
+    }
+
+    const jokes = await Joke.findAll({
+      where,
+    });
+
     res.send(jokes);
   } catch (error) {
     console.error(error);
